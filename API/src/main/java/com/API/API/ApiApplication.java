2ee2c71addc116f09f9252d3c3242e17010de360
Airhoax <59318969+Airhoax@users.noreply.github.com>
2020-07-07 17:28:38 +0200

    Add files via upload

---
+++ API/src/main/java/com/API/API/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.API.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApiCode.class, args);
+	}
+
+}
